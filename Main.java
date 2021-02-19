1889c944c790bb6366ef33f904228340b357aa89
Kristian Smolko <kristian.smolko@kosickaakademia.sk>
2021-02-19 18:17:07 +0100

    Made some changes

--- Main.java
+++ Main.java
@@ -1,9 +1,12 @@
 public class Main {
     public static void main(String[] argstr) {
         // this is a comment
+        boolean falseBool = false;
     }
+
     public void hello()
     {
         // xxx
+        System.out.println("Hello");
     }
 }
