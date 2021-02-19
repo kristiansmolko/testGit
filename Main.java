813940d4cded45469d485e59ec57d93e5739e6ba
kosickaakademia <72263463+kosickaakademia@users.noreply.github.com>
2021-02-19 14:35:20 +0100

    Update Main.java
    changed

--- Main.java
+++ Main.java
@@ -1,5 +1,9 @@
 public class Main {
-    public static void main(String[] args) {
-
+    public static void main(String[] argstr) {
+        // this is a comment
+    }
+    public void hello()
+    {
+        // xxx
     }
 }
